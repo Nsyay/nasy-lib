628fc161bccd1ad70d7f9a643a608c775ce8b3e4
Nasya Ayu <ayu.siti21@ui.ac.id>
2023-09-19 22:07:39 +0700

    tugas 3 sementara

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
+class Book(models.Model):
     name = models.CharField(max_length=255, default="Book Title")
     author = models.TextField(default="-")
     amount = models.IntegerField(default=0)
     genre = models.TextField(default="-")
     description = models.TextField(default="-")
+    date_added = models.DateField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -1,13 +1,43 @@
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.core import serializers
+from main.forms import BookForm
+from django.urls import reverse
 from django.shortcuts import render
+from main.models import Book
+
 
 # Create your views here.
 def show_main(request):
+    books = Book.objects.all()
     context = {
-        'name' : 'The 48 Laws of Power',
-        'author' : 'Robert Greene',
-        'amount' : 48,
-        'genre' : 'non-fiction',
-        'description' : 'evil book'
+        'books' : books
     }
 
     return render(request, "main.html", context)
+
+def book_entry(request):
+    form = BookForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "book_entry.html", context)
+
+def show_xml(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Book.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Book.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")\
+
+def show_json_by_id(request, id):
+    data = Book.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
